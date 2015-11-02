566eb7d71203b62658d04a4fbf22b41d95228a21
mydongistiny <jaysonedson@gmail.com>
2015-11-02 04:54:20 -0800

    skia: Change to arm mode

--- Android.mk
+++ Android.mk
@@ -41,7 +41,7 @@ ifneq ($(strip $(TARGET_FDO_CFLAGS)),)
 	LOCAL_CFLAGS += -O2
 endif
 
-LOCAL_ARM_MODE := thumb
+LOCAL_ARM_MODE := arm
 ifeq ($(TARGET_ARCH),arm)
 	ifeq ($(ARCH_ARM_HAVE_VFP),true)
 		LOCAL_CFLAGS += -DANDROID_LARGE_MEMORY_DEVICE
